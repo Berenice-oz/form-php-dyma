4945971e8b213dfde361e04f8199a6d9150808ab
beren <beren@localhost>
2022-04-12 16:22:27 +0200

    form dyma exercice

---
+++ index.php
@@ -0,0 +1,13 @@
+<pre>
+<?php
+
+print_r($_GET);
+?>
+</pre>
+<form action="index.php" method="">
+  <div>
+    <label for="name">Nom</label>
+    <input type="text" name="name" id="name">
+  </div>
+  <button type="submit">Submit</button>
+</form>
